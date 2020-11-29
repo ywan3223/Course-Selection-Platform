072d9fc3b02d2eb53a7006677ce5be8d35327b84
ywan3223 <wwwinniewang@gmail.com>
2020-11-29 14:23:32 -0500

    add content coponent into angular

---
+++ front-end/app/src/app/request.ts
@@ -0,0 +1,35 @@
+import { getSignature } from './localstorage';
+
+const baseUrl = 'http://localhost:3003';
+export function request(
+  url: string,
+  config: any,
+  auth: boolean = true
+): Promise<any> {
+  const completeUrl = baseUrl + url;
+  config.headers = Object.assign(
+    {},
+    {
+      'content-type': 'application/json',
+    }
+  );
+  const signature = getSignature();
+  if (auth && signature) {
+    config.headers.Authorization = 'Bearer ' + signature;
+  }
+  return new Promise((resolve, reject) => {
+    fetch(completeUrl, config)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (config.method !== 'GET') {
+          alert(res.msg);
+        }
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
